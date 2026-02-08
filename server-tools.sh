8ab5f15c0949f3c582ce3104fd0aedc47c7ec71b
vahidyeganeh1373 <124838058+vahidyeganeh1373@users.noreply.github.com>
2026-02-09 03:01:19 +0330

    Update server-tools.sh

--- server-tools.sh
+++ server-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION="1.2.2"
+VERSION="1.2.1"
 SERVER_IP=$(hostname -I | awk '{print $1}')
 
 # Colors
@@ -160,8 +160,7 @@ ExecStart=/usr/bin/autossh -M 0 -g \\
     -o "ServerAliveInterval=20" \\
     -o "ServerAliveCountMax=3" \\
     -o "Cipher=chacha20-poly1305@openssh.com" \\
-    -p \${REMOTE_SSH_PORT} \\
-    -L 0.0.0.0:\${CONFIG_PORT}:127.0.0.1:\${CONFIG_PORT} \\
+    -p \${REMOTE_SSH_PORT} -L 0.0.0.0:\${CONFIG_PORT}:127.0.0.1:\${CONFIG_PORT} \\
     root@\${FOREIGN_IP} "sleep infinity"
 Restart=always
 RestartSec=5
