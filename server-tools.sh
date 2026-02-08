a755ed419a989b686d085ef4b315286f1b55133a
vahidyeganeh1373 <124838058+vahidyeganeh1373@users.noreply.github.com>
2026-02-09 03:17:10 +0330

    Update server-tools.sh

--- server-tools.sh
+++ server-tools.sh
@@ -474,7 +474,7 @@ function main_menu() {
     echo -e "${CYAN}07)${NC} Backhual Tunnel (Premium)"
     echo -e "${CYAN}08)${NC} GOST Tunnel"
     echo -e "${CYAN}09)${NC} Lena Tunnel"
-    echo -e "${CYAN}10)${NC} Auto SSH Tunnel (Just Iran Side)"
+    echo -e "${CYAN}10)${NC} Auto "SSH + Cipher + Mux" Tunnel (Just Iran Side)"
     echo -e "${CYAN}11)${NC} Pasarguard Node"
     echo -e "${CYAN}12)${NC} Enable Root Login"
     echo -e "${CYAN}00)${NC} Exit"
